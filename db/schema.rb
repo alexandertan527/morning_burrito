16f84cb14205bca9c0543556224f966858861a5d
Patrick Amfo <patrick.amfo@icloud.com>
2025-04-03 19:05:07 +0100

    adding video to seed file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_18_211248) do
+ActiveRecord::Schema[7.1].define(version: 2025_04_01_202816) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,6 +49,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_18_211248) do
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "hint"
   end
 
   create_table "user_challenges", force: :cascade do |t|
